14e480a323e551084032f183b667ae989a345406
UPinar <uygrpinar@gmail.com>
2024-09-27 18:10:13 +0300

    lesson_31_added

--- Lesson_29/main.c
+++ Lesson_29/main.c
@@ -1239,3 +1239,888 @@
     // using indeterminate value is undefined behavior
   }
 */
+
+/*
+                    -----------------------
+                    | pointer arithmetics |
+                    -----------------------
+*/
+
+/*
+  LEGAL OPERATIONS
+  --------------------
+  -> address + integer  = address
+  -> integer + address  = address
+  -> address - integer  = address
+  -> address - address  = signed integer
+
+  ILLEGAL OPERATIONS
+  ---------------------
+  -> integer - address
+  -> address + address
+*/
+
+/*
+  int main(void)
+  {
+    int a[10] = { 0 };
+
+    a + 5;  // legal    (address + integer)
+    3 + a;  // legal    (integer + address)
+
+    int* p = a + 7;
+    p - 5;  // legal    (address - integer)
+    p + 1;  // legal    (address + integer)
+
+    p + a;  // illegal  (address + address)
+    3 - a;  // illegal  (integer - address)
+  }
+*/
+
+/*
+  - Bir dizinin 5 indisli elemaının adresi ile 1'i toplarsak 
+    o dizinin 6 indisli elemanının adresini elde ederiz.
+    &a[5] + 1 = &a[6]
+*/
+
+/*
+  int main(void)
+  {
+    char a[10] = { 0 };
+
+    for (int i = 0; i < 10; ++i)
+      printf("%p\n", a + i);
+
+    // output ->
+    //  00000028D9FFFE22
+    //  00000028D9FFFE23  
+    //  00000028D9FFFE24
+    //  00000028D9FFFE25
+    //  00000028D9FFFE26
+    //  00000028D9FFFE27
+    //  00000028D9FFFE28
+    //  00000028D9FFFE29
+    //  00000028D9FFFE2A
+    //  00000028D9FFFE2B
+    // addresses are increasing by 1
+  }
+*/
+
+/*
+  int main(void)
+  {
+    short a[10] = { 0 };
+
+    for (int i = 0; i < 10; ++i)
+      printf("%p\n", a + i);
+    
+    // output ->
+    //  000000E4B6DFFC30
+    //  000000E4B6DFFC32
+    //  000000E4B6DFFC34
+    //  000000E4B6DFFC36
+    //  000000E4B6DFFC38
+    //  000000E4B6DFFC3A
+    //  000000E4B6DFFC3C
+    //  000000E4B6DFFC3E
+    //  000000E4B6DFFC40
+    //  000000E4B6DFFC42
+    // addresses are increasing by 2
+  }
+*/
+
+/*
+  int main(void)
+  {
+    int a[10] = { 0 };
+
+    for (int i = 0; i < 10; ++i)
+      printf("%p\n", a + i);
+    
+    // output ->
+    //  00000034CEFFF900
+    //  00000034CEFFF904
+    //  00000034CEFFF908
+    //  00000034CEFFF90C
+    //  00000034CEFFF910
+    //  00000034CEFFF914
+    //  00000034CEFFF918
+    //  00000034CEFFF91C
+    //  00000034CEFFF920
+    //  00000034CEFFF924
+    // addresses are increasing by 4
+  }
+*/
+
+/*
+  int main(void)
+  {
+    double a[10] = { 0 };
+
+    for (int i = 0; i < 10; ++i)
+      printf("%p\n", a + i);
+    
+    // output ->
+    //  0000006AB49FF730
+    //  0000006AB49FF738
+    //  0000006AB49FF740
+    //  0000006AB49FF748
+    //  0000006AB49FF750
+    //  0000006AB49FF758
+    //  0000006AB49FF760
+    //  0000006AB49FF768
+    //  0000006AB49FF770
+    //  0000006AB49FF778
+    // addresses are increasing by 8
+  }
+*/
+
+/*
+  int main(void)
+  {
+    int a[10] = { 0, 11, 22, 33, 44, 55, 66, 77, 88, 99 };
+
+    // &a[3] -> (a + 3)
+
+    for(int i = 0; i < 10; ++i)
+      printf("%2d %2d %2d\n", a[i], *(a + i), *(i + a));
+
+    // output ->
+    //   0  0  0
+    //  11 11 11
+    //  22 22 22
+    //  33 33 33
+    //  44 44 44
+    //  55 55 55
+    //  66 66 66
+    //  77 77 77
+    //  88 88 88
+    //  99 99 99
+  }
+*/
+
+/*
+  int main(void)
+  {
+    int a[10] = { 0, 11, 22, 33, 44, 55, 66, 77, 88, 99 };
+
+    int* ptr = a;
+    printf("%d\n", *ptr); // output -> 0
+
+    ++ptr;
+    printf("%d\n", *ptr); // output -> 11
+
+    ptr++; 
+    printf("%d\n", *ptr); // output -> 22
+
+    ptr += 1;
+    printf("%d\n", *ptr); // output -> 33
+
+    ptr = ptr + 1;
+    printf("%d\n", *ptr); // output -> 44
+  }
+*/
+
+/*
+  int main(void)
+  {
+    int a[10] = { 0, 11, 22, 33, 44, 55, 66, 77, 88, 99 };
+
+    int* ptr = a;
+    printf("%d\n", *ptr); // output -> 0
+
+    ptr += 5;
+    printf("%d\n", *ptr); // output -> 55
+  }
+*/
+
+/*
+  int main(void)
+  {
+    int a[10] = { 0, 11, 22, 33, 44, 55, 66, 77, 88, 99 };
+    int* ptr = a;
+
+    for (int i = 0; i < 10; ++i){
+      printf("%2d %2d %2d %2d\n", a[i], *(a + i), *(i + a), *ptr);
+      ++ptr;
+    }
+
+    // output ->
+    //   0  0  0  0
+    //  11 11 11 11
+    //  22 22 22 22
+    //  33 33 33 33
+    //  44 44 44 44
+    //  55 55 55 55
+    //  66 66 66 66
+    //  77 77 77 77
+    //  88 88 88 88
+    //  99 99 99 99
+  }
+*/
+
+/*
+  int main(void)
+  {
+    int a[10] = { 0, 11, 22, 33, 44, 55, 66, 77, 88, 99 };
+    int* ptr = a;
+
+    int n  = 10;
+    while(n--){
+      printf("%d ", *ptr);
+      ++ptr;
+    }
+    // output -> 0 11 22 33 44 55 66 77 88 99
+  }
+*/
+
+/*
+  int main(void)
+  {
+    int a[10] = { 0, 11, 22, 33, 44, 55, 66, 77, 88, 99 };
+
+    int* ptr_1 = a + 3;
+    int* ptr_2 = a + 8;
+
+    printf("%d\n", ptr_2 - ptr_1);  // output -> 5
+    printf("%d\n", ptr_1 - ptr_2);  // output -> -5
+
+    // warning: format '%d' expects argument of type 'int', 
+    // but argument 2 has type 'long long int
+
+    // "ptr_2 - ptr_1" expression's data type is ptrdiff_t(type alias)
+
+    // ptrdiff_t is a signed integer type and implementation-defined
+    // because of gcc compiler implemented ptrdiff_t as long long int
+    // we are getting this warning
+  }
+*/
+
+/*
+            --------------------------------------------------
+            | subscript operator (köşeli parantez operatörü) |
+            --------------------------------------------------
+*/
+
+/*
+  - binary operator
+  - 1st priority level operator (left associative)
+
+    x[y]  --> *(x + y)
+*/
+
+/*
+  int main(void)
+  {
+    int x = 6;
+    printf("%d\n", x);    // output -> 6
+
+
+    (&x)[0] = 99;   // (&x)[0] ===> *(&x + 0)
+    printf("%d\n", x);    // output -> 99
+  }
+*/
+
+/*
+  int main(void)
+  {
+    int x = 5;
+    printf("x = %d\n", x);  // output -> x = 5
+
+    int* ptr = &x;
+    ptr[0] = 88;   // ptr[0] ===> *(ptr + 0)
+    printf("x = %d\n", x);  // output -> x = 88
+  }
+*/
+
+/*
+  int main(void)
+  {
+    int a[10] = { 0 , 11, 22, 33, 44, 55, 66, 77, 88, 99 };
+    int* p = a + 5;
+
+    printf("%d\n", *p);     
+    // output -> 55
+
+    printf("%d\n", p[0]);   
+    // output -> 55  (p[0]  ===> *(p + 0))
+
+    printf("%d\n", p[3]);   
+    // output -> 88  (p[3]  ===> *(p + 3))
+
+    printf("%d\n", p[-4]);  
+    // output -> 11  (p[-4] ===> *(p - 4))
+    
+    printf("%d\n", 2[p]);   
+    // output -> 77  (2[p]  ===> *(2 + p))
+  }
+*/
+
+/*
+  int main(void)
+  {
+    int a[10] = { 0 , 11, 22, 33, 44, 55, 66, 77, 88, 99 };
+    int* ptr = a + 5;
+
+    int k = ptr[-4];
+  }
+
+  // compiled with x86-64 gcc 14.2 -std=c11 -O0
+  //  main:
+  //    push rbp
+  //    mov rbp, rsp
+  //    mov DWORD PTR [rbp-64], 0       : a[0] = 0 (a[0] is DWORD(int))
+  //    mov DWORD PTR [rbp-60], 11
+  //    mov DWORD PTR [rbp-56], 22
+  //    mov DWORD PTR [rbp-52], 33
+  //    mov DWORD PTR [rbp-48], 44
+  //    mov DWORD PTR [rbp-44], 55
+  //    mov DWORD PTR [rbp-40], 66
+  //    mov DWORD PTR [rbp-36], 77
+  //    mov DWORD PTR [rbp-32], 88
+  //    mov DWORD PTR [rbp-28], 99
+  //    lea rax, [rbp-64]               : rax = &a[0]
+  //    add rax, 20                     : rax = &a[0] + 5*4[20byte]
+  //    mov QWORD PTR [rbp-8], rax      : ptr = &a[5]
+  //    mov rax, QWORD PTR [rbp-8]      : rax = ptr
+  //    mov eax, DWORD PTR [rax-16]     : eax = *(ptr - 4*4[16byte])
+  //    mov DWORD PTR [rbp-12], eax     : int k = eax 
+  //    mov eax, 0
+  //    pop rbp
+  //    ret
+*/
+
+/*
+  int main(void)
+  {
+    int a[10] = { 0 , 11, 22, 33, 44, 55, 66, 77, 88, 99 };
+    int* p = a;
+
+    p += 3;       // p = &a[0] + 3  ===> p = (a + 3)
+    p[-2] = 999;  // p[-2] = *(p - 2) = *(a + 3 - 2) = a[1] = 999
+
+    printf("%d\n", a[1]);  // output -> 999
+  }
+*/
+
+/*
+  int main(void)
+  {
+    int a[10] = { 0 , 11, 22, 33, 44, 55, 66, 77, 88, 99 }; 
+
+    //------------------------------------------------------
+
+    for(int i = 0; i < 10; ++i)
+      printf("%d ", a[i]);
+    // output -> 0 11 22 33 44 55 66 77 88 99
+
+    printf("\n");
+
+    //------------------------------------------------------
+    
+    int* p = a;
+    for(int i = 0; i < 10; ++i)
+      printf("%d ", p[i]);
+    // output -> 0 11 22 33 44 55 66 77 88 99
+
+    printf("\n");
+
+    //------------------------------------------------------
+
+    int size = 10;
+    while(size--){
+      printf("%d ", *p);
+      ++p;
+    }
+    // output -> 0 11 22 33 44 55 66 77 88 99
+      
+    //------------------------------------------------------
+  }
+*/
+
+/*  
+            ---------------------------------------------
+            | diziler üzerinde işlem yapan fonksiyonlar |
+            ---------------------------------------------
+            |     functions that operate on arrays      |
+            ---------------------------------------------
+*/
+
+/*
+  void foo(int* ptr)
+  {
+    for(int i = 0; i < sizeof(ptr) / sizeof(ptr[0]); ++i)
+      printf("%d ", ptr[i]);
+
+    // sizeof(ptr) is 8 for x64 systems
+    // sizeof(ptr[0]) is 4 for int type
+
+    // for(int i = 0; i < 2; ++i)
+    //    printf("%d ", ptr[i]);
+  }
+
+  int main(void)
+  {
+    int a[10] = { 0 , 11, 22, 33, 44, 55, 66, 77, 88, 99 };
+
+    for(int i = 0; i < sizeof(a) / sizeof(a[0]); ++i)
+      printf("%d ", a[i]);
+
+    // output -> 0 11 22 33 44 55 66 77 88 99
+
+    foo(a); // output -> 0 11
+  }
+*/
+
+/*
+  void foo(int* ptr, int size)
+  {
+    // <---- WAY 1 ---->
+
+    for(int i = 0; i < size; ++i)
+      printf("%d ", ptr[i]);
+
+    printf("\n");
+
+    // ptr variable's value is not changed
+
+    // <---- WAY 2 ---->
+
+    while(size--){
+      printf("%d ", *ptr);
+      ++ptr;
+    }
+
+    // ptr variable's value is the address of end of the array
+  }
+*/
+
+/*
+  void aprint_1(int* ptr, int size)
+  {
+    for(int i = 0; i < size; ++i)
+      printf("%d ", ptr[i]);
+
+    printf("\n");
+  }
+
+  void aprint_2(int* ptr, int size)
+  {
+      while(size--){
+        printf("%d ", *ptr);
+        ++ptr;
+      }
+      printf("\n");
+  }
+
+  int main(void)
+  {
+    int a[10] = { 0 , 11, 22, 33, 44, 55, 66, 77, 88, 99 };
+
+    aprint_1(a, 10);  // output -> 0 11 22 33 44 55 66 77 88 99
+    aprint_2(a, 10);  // output -> 0 11 22 33 44 55 66 77 88 99
+
+    aprint_1(a, 4);   // output -> 0 11 22 33
+
+    // Question: call aprint_1 function to output a[3], a[4], a[5]
+    aprint_1(a + 2, 3);  // output -> 22 33 44
+    aprint_1(&a[2], 3);  // output -> 22 33 44
+
+    // Question: call aprint_2 function to output last 4 elements
+    aprint_2(a + 6, 4);  // output -> 66 77 88 99
+    aprint_2(&a[6], 4);  // output -> 66 77 88 99
+  } 
+*/
+
+/*
+  void foo(const int ival);
+  void foo(int ival);
+  // function redeclaration
+  - nothing change for function's caller
+
+  difference in pointer arguments constness is important 
+  but difference of non-pointer arguments constness is not important
+*/
+
+/*
+  #include "../nutility.h"
+
+  #define   SIZE  100
+
+  int get_sum_1(const int* p_array, int size)
+  {
+    int sum = 0;
+    for(int i = 0; i < size; ++i)
+      sum += p_array[i];
+    
+    return sum;
+  }
+
+  int get_sum_2(const int* p_array, int size)
+  {
+    int sum = 0;
+    while (size--){
+      sum += *p_array;
+      ++p_array;
+    }
+    
+    return sum;
+  }
+
+  int main(void)
+  {
+    int a[SIZE];
+
+    randomize();
+    set_array_random_1(a, SIZE);
+
+    printf("sum array = %d\n", get_sum_1(a, SIZE));
+    // sum array = 54941
+  }
+*/
+
+/*
+  #include "../nutility.h"
+
+  #define   SIZE  100
+
+  double get_mean(const int* p_array, int size)
+  {
+    return (double)get_sum(p_array, size) / size;
+  }
+
+  int main(void)
+  {
+    int a[SIZE];
+
+    randomize();
+    set_array_random(a, SIZE);
+
+    printf("mean = %f\n", get_mean(a, SIZE));
+    // mean = mean = 530.040000
+  }
+*/
+
+/*
+  #include "../nutility.h"
+  #include <math.h>
+
+  #define   SIZE  100
+
+  // standart deviation
+  double get_std_dev(const int* p_array, int size)
+  {
+    double mean = get_mean(p_array, size);
+    double sum_square = 0.;
+
+    for(int i = 0; i < size; ++i)
+      sum_square += (p_array[i] - mean) * (p_array[i] - mean);
+
+    return sqrt(sum_square / (size - 1));
+  }
+
+  int main(void)
+  {
+    int a[SIZE];
+
+    randomize();
+    set_array_random(a, SIZE);
+
+    double std_dev = get_std_dev(a, SIZE);
+    printf("standart deviation = %f\n", std_dev);
+    // output -> standart deviation = 276.183150
+  }
+*/
+
+/*
+  #include "../nutility.h"
+
+  #define   SIZE  20
+
+  void reverse_array(int* p_array, int size)
+  {
+    for(int i = 0; i < size / 2; ++i)
+      swap(&p_array[i], &p_array[size - i - 1]);
+  }
+
+  int main(void)
+  {
+    int a[SIZE];
+
+    randomize();
+    set_array_random(a, SIZE);
+
+    print_array(a, SIZE); 
+    // output ->
+    //  424 314 297 657 734 384 669 623 736 798
+    //  703 587 605 390 681 768 213  19 330 906
+
+    reverse_array(a, SIZE);
+    print_array(a, SIZE);
+    // output ->
+    //  906 330  19 213 768 681 390 605 587 703
+    //  798 736 623 669 384 734 657 297 314 424
+  }
+*/
+
+/*
+  #include "../nutility.h"
+
+  #define   SIZE  20
+
+  int get_max_elem(const int* p_array, int size)
+  {
+    int max = *p_array;
+    for(int i = 1; i < size; ++i)
+      if(p_array[i] > max)
+        max = p_array[i];
+    
+    return max;
+  }
+
+  int get_min_elem(const int* p_array, int size)
+  {
+    int min = *p_array;
+    for(int i = 1; i < size; ++i)
+      if(p_array[i] < min)
+        min = p_array[i];
+    
+    return min;
+  }
+
+  int main(void)
+  {
+    int a[SIZE];
+
+    randomize();
+    set_array_random(a, SIZE);
+
+    print_array(a, SIZE);
+    // output ->
+    //  540 253 228 542 375 838 551 535 836 311
+    //  887 883 692 370 225 387 927 989 844 183
+
+    int max = get_max_elem(a, SIZE);
+    printf("max elem = %d\n", max);
+    // output -> max elem = 989
+
+    int min = get_min_elem(a, SIZE);
+    printf("min elem = %d\n", min);
+    // output -> min elem = 183
+  }
+*/
+
+/*
+  #include "../nutility.h"
+
+  #define   SIZE  20
+
+  // 2 in param   : p_array, size
+  // 2 out param  : min, max
+  void get_min_max(const int* p_array, int size, int* p_min, int* p_max)
+  {
+    *p_min = *p_max = *p_array;
+
+    for(int i = 1; i < size; ++i){
+      if(p_array[i] > *p_max)
+        *p_max = p_array[i];
+      else if(p_array[i] < *p_min)
+        *p_min = p_array[i];
+    }
+  }
+
+  int main(void)
+  {
+    int a[SIZE];
+
+    randomize();
+    set_array_random(a, SIZE);
+
+    print_array(a, SIZE);
+    // output ->
+    //  444 886 816 750 228 361 870 638 301 700
+    //  962 794  65 318 895 182 481 991 809 274
+
+    int min, max;
+
+    get_min_max(a, SIZE, &min, &max);
+    printf("min = %d, max = %d\n", min, max);
+    // output -> min = 65, max = 991
+  }
+*/
+
+/*
+  #include "../nutility.h"
+
+  #define  SIZE  20
+
+  void copy_array_1(int* p_dest, const int* p_source, int size)
+  {
+    for(int i = 0; i < size; ++i)
+      p_dest[i] = p_source[i];
+  }
+
+  void copy_array_2(int* p_dest, const int* p_source, int size)
+  {
+    while (size--)
+      *p_dest++ = *p_source++;
+  }
+
+  int main(void)
+  {
+    int a[SIZE];
+    int b[SIZE];
+
+    randomize();
+    set_array_random(a, SIZE);
+
+    print_array(a, SIZE);
+    // output ->
+    //  119  26 864 694 188 926 925 506 603 469
+    //  622 390 580 671 979 495 551 179 696 806
+
+    copy_array_1(b, a, SIZE);
+    print_array(b, SIZE);
+    // output ->
+    //  119  26 864 694 188 926 925 506 603 469
+    //  622 390 580 671 979 495 551 179 696 806
+  }
+*/
+
+/*
+  #include "../nutility.h"
+
+  #define  SIZE  40
+
+  int main(void)
+  {
+    int a[SIZE];
+    int b[SIZE] = { 0 };
+
+    randomize();
+    set_array_random(a, SIZE);
+
+    int idx_a = 10, idx_b = 20, n = 10;
+
+    printf("array a : \n");
+    print_array(a, SIZE);
+
+    // output ->
+    //  array a :
+    //  938 112 735 848   6 658 857 414 751 635
+    //  542 156 398 167 477 251 965 644 599 847
+    //  350 804 667 912 361 786 566  52 116 187
+    //  305 970 912  47 411 242 866 127 776 868
+
+    copy_array(b + idx_b, a + idx_a, n);
+
+    printf("array b : \n");
+    print_array(b, SIZE);
+    // output ->
+    //  array b :
+    //    0   0   0   0   0   0   0   0   0   0
+    //    0   0   0   0   0   0   0   0   0   0
+    //  542 156 398 167 477 251 965 644 599 847
+    //    0   0   0   0   0   0   0   0   0   0
+  }
+*/
+
+/*
+  #include "../nutility.h"
+
+  #define SIZE 20
+
+  void bubble_sort(int* p, int size)
+  {
+    for (int i = 0; i < size - 1; ++i){
+      for (int k = 0; k < size - i - 1; ++k){
+        if (p[k] > p[k + 1])
+          swap(p + k, p + k + 1);
+      }
+    }
+  }
+
+  int main(void)
+  {
+    int a[SIZE];
+
+    randomize();
+    set_array_random(a, SIZE);
+    print_array(a, SIZE);
+    // output ->
+    //  405  39 408 262 430 125 185 797 612 198
+    //  392 455 529 995 461 998  82  11 248 771
+
+    bubble_sort(a, SIZE);
+    print_array(a, SIZE);
+    // output ->
+    //   11  39  82 125 185 198 248 262 392 405
+    //  408 430 455 461 529 612 771 797 995 998
+
+    set_array_random(a, SIZE);
+    bubble_sort(a, SIZE - 10);  // sort first line(10 elements)
+    print_array(a, SIZE);
+    // output ->
+    //    8  48  83 172 240 331 356 417 512 586
+    //  575   7 771 537 941 587  24 348 365 846
+
+    set_array_random(a, SIZE);
+    bubble_sort(a + 10, SIZE - 10);  // sort second line(10 elements)
+    print_array(a, SIZE);
+    // output ->
+    //  515 813  21 432   3 295 608 164 492 556
+    //   17 152 247 534 573 723 730 843 859 902
+  }
+*/
+
+/*
+  #include "../nutility.h"
+
+  #define SIZE 20
+
+  void swap_arrays(int* p_array1, int* p_array2, int size)
+  {
+    while (size--)
+      swap(p_array1++, p_array2++);
+  }
+
+  int main(void)
+  {
+    int a[SIZE];
+    int b[SIZE];
+
+    randomize();
+    set_array_random(a, SIZE);
+    set_array_random(b, SIZE);
+
+    print_array(a, SIZE);
+    print_array(b, SIZE);
+    // output ->
+    //  470 843 424 350 878 287  36 778 439 776
+    //  710  46  74 378 377 352 692 322 238 101
+    //  ---------------------------------------
+    //   38 868 501 190 644  10  35 668 224 526
+    //  492 399 967 682 746 685 972  86 646 705
+    //  ---------------------------------------
+
+    swap_arrays(a, b, SIZE);
+    print_array(a, SIZE);
+    print_array(b, SIZE);
+    // output ->
+    //   38 868 501 190 644  10  35 668 224 526
+    //  492 399 967 682 746 685 972  86 646 705
+    //  ---------------------------------------
+    //  470 843 424 350 878 287  36 778 439 776
+    //  710  46  74 378 377 352 692 322 238 101
+    //  ---------------------------------------
+  }
+*/
+
+/*
+  void func(int* p, int size);
+  void func(int p[], int size);
+
+  // there is an alternative syntax
+  // but this syntax is only for functions, 
+  // which have pointers as parameters
+*/
--- nutility.c
+++ nutility.c
@@ -3,21 +3,37 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <math.h>
 
 void randomize(void){
   srand((unsigned)time(NULL));
 }
 
-void set_array_random(int* pArr, int size){
-  while (size--)
-    *pArr++ = rand() % 1000;
+void sgets(char* pStr){
+  int c;
+  while ((c = getchar()) != '\n')
+    *pStr++ = (char)c;
+  *pStr = '\0';
 }
 
-void print_array(const int* pArr, int size){
+void swap(int* x, int* y){
+  int temp = *x;
+  *x = *y;
+  *y = temp;
+}
+
+void set_array_random(int* p_array, int size){
+  while(size--){
+    *p_array = rand() % 1000;
+    ++p_array;
+  }
+}
+
+void print_array(const int* p_array, int size){
   for (int i = 0; i < size; ++i){
     if (i && i % 10 == 0)
       putchar('\n');
-    printf("%3d ", pArr[i]);
+    printf("%3d ", p_array[i]);
   }
   printf("\n---------------------------------------\n");
 }
@@ -34,15 +50,77 @@ void sort_array(int* pArr, int size){
   }
 }
 
-void sgets(char* pStr){
-  int c;
-  while ((c = getchar()) != '\n')
-    *pStr++ = (char)c;
-  *pStr = '\0';
+void reverse_array(int* p_array, int size)
+{
+  for(int i = 0; i < size / 2; ++i)
+    swap(p_array + i, p_array + size - i - 1);
 }
 
-void swap(int* x, int* y){
-  int temp = *x;
-  *x = *y;
-  *y = temp;
+int get_sum(const int* p_array, int size)
+{
+  int sum = 0;
+  for(int i = 0; i < size; ++i)
+    sum += p_array[i];
+  
+  return sum;
+}
+
+double get_mean(const int* p_array, int size)
+{
+  return (double)get_sum(p_array, size) / size;
+}
+
+double get_std_dev(const int* p_array, int size)
+{
+  double mean = get_mean(p_array, size);
+  double sum_square = 0.;
+
+  for(int i = 0; i < size; ++i)
+    sum_square += (p_array[i] - mean) * (p_array[i] - mean);
+
+  return sqrt(sum_square / (size - 1));
+}
+
+int get_max_elem(const int* p_array, int size)
+{
+  int max = p_array[0];
+  for(int i = 1; i < size; ++i)
+    if(p_array[i] > max)
+      max = p_array[i];
+  
+  return max;
+}
+
+int get_min_elem(const int* p_array, int size)
+{
+  int min = *p_array;
+  for(int i = 1; i < size; ++i)
+    if(p_array[i] < min)
+      min = p_array[i];
+  
+  return min;
+}
+
+void get_min_max(const int* p_array, int size, int* p_min, int* p_max)
+{
+  *p_min = *p_max = *p_array;
+
+  for(int i = 1; i < size; ++i){
+    if(p_array[i] > *p_max)
+      *p_max = p_array[i];
+    else if(p_array[i] < *p_min)
+      *p_min = p_array[i];
+  }
+}
+
+void copy_array(int* p_dest, const int* p_source, int size)
+{
+  while (size--)
+    *p_dest++ = *p_source++;
+}
+
+void swap_arrays(int* p_array1, int* p_array2, int size)
+{
+  while (size--)
+    swap(p_array1++, p_array2++);
 }
